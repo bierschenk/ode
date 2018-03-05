9c5c0ce6266e485bd6af8872e6e04acb4fbbc04e
bierschenk <bierschenk.devel@gmail.com>
2018-03-05 08:46:18 -0600

    change setup metadata

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author_email='bierschenk.devel@gmail.com',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering'
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
