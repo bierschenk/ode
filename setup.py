21140a93732e1c9745b3dea2e8951890774f6c7b
bierschenk <bierschenk.devel@gmail.com>
2019-07-20 16:24:48 -0500

    bumpversion 0.3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     description="Ordinary differential equation solver (numeric integration)",
     long_description=readme + '\n\n' + history,
     keywords='ode',
-    version='0.2.0',
+    version='0.3.0',
     license="MIT license",
     packages=['ode'],
     include_package_data=True,
