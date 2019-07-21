6b7440b3ad7fc9cd8d43c0994654e4d8cf4668d0
bierschenk <bierschenk.devel@gmail.com>
2019-07-21 01:22:27 -0500

    fix doc config path and description

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -156,8 +156,8 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
     (master_doc, 'ode', 'ode Documentation',
-     author, 'ode', 'One line description of project.',
-     'Miscellaneous'),
+     author, 'ode', 'Numeric integration for ordinary differential equations',
+     'Scientific/Engineering'),
 ]
 
 
